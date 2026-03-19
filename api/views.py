8fa4ff0f114315341a91cac60470b0caecdf6a6d
saulnyongesa <saulnyongesa4@gmail.com>
2026-03-20 01:48:24 +0300

    done

--- api/views.py
+++ api/views.py
@@ -317,7 +317,7 @@ class AdviceMessageViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return AdviceMessage.objects.filter(
-            profile=self.request.user     
+            profile=self.request.user.adolescent_profile
         ).order_by('created_at')
 
 
